5d14bc9d2d7de22053992c430fadafcc451db446
mariano <mariano@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-05-20 13:40:43 +0000

    Changed name from WDPartners to LaborGuru

---
+++ project/LaborGuru_SPM/common/src/test/java/com/mindpool/laborguru/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.mindpool.laborguru.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
