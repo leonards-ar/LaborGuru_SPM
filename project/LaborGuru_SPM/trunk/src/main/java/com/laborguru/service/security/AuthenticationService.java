adacd1b6538a3ed27c2b2c8d9fe0608542d25df9
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-06-13 12:15:54 +0000

    Error Control added to the AuthenticationService

--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/service/security/AuthenticationService.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/service/security/AuthenticationService.java
@@ -1,36 +1,48 @@
 package com.laborguru.service.security;
 
-import java.util.Set;
+import java.security.Principal;
+
+import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.springframework.dao.DataAccessException;
 
 import com.laborguru.model.User;
 import com.laborguru.service.user.UserService;
-import com.laborguru.utils.UserAdapter;
-import com.mindpool.security.principal.SecurityUser;
+import com.laborguru.utils.UserDetailsImpl;
 import com.mindpool.security.service.UserAuthenticationService;
 
-public class AuthenticationService implements UserAuthenticationService{
+public class AuthenticationService implements UserAuthenticationService {
 
 	UserService service;
-	
-	public SecurityUser authenticateUser(String username, String password)
-			throws Exception {
-		User user = new User();
-		user.setUserName(username);
-		user.setPassword(password);
-		return new UserAdapter(service.authenticateUser(user));
-	}
 
-	public Set<String> getPermissions(SecurityUser user) {
-		return user.getPermissions();
-	}
+	private String reason = null;
 
-	public Set<String> getRoles(SecurityUser user) {
-		return user.getRoles();
-	}
-	
-	public void setService(UserService service){
+	public void setService(UserService service) {
 		this.service = service;
 	}
-	
+
+	public Principal authenticate(String username, String password)
+			throws UsernameNotFoundException, DataAccessException {
+		UserDetailsImpl userDetails = null;
+		User user = new User();
+		user.setUserName(username);
+		user = service.getUserByUserName(user);
+
+		if (user != null) {
+			if (password.equals(user.getPassword())) {
+				userDetails = new UserDetailsImpl(user);
+			} else {
+				reason = UserAuthenticationService.BAD_PASSWORD;
+			}
+
+		} else {
+			reason = UserAuthenticationService.UNKNOWN_USER_ERROR;
+		}
+
+		return userDetails;
+	}
+
+	public String getReason() {
+		return reason;
+	}
 
 }
