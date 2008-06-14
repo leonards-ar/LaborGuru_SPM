a713be6070c12a6208fd9b12736e298159f3daee
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2008-06-14 23:39:44 +0000

    Added methods hash and equals.

--- src/main/java/com/laborguru/model/Employee.java
+++ src/main/java/com/laborguru/model/Employee.java
@@ -107,6 +107,107 @@ public class Employee extends User {
 	public void setPosition(Position position) {
 		this.position = position;
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result
+				+ ((address2 == null) ? 0 : address2.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result
+				+ ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result
+				+ ((hireDate == null) ? 0 : hireDate.hashCode());
+		result = prime * result
+				+ ((homePhone == null) ? 0 : homePhone.hashCode());
+		result = prime * result + (manager ? 1231 : 1237);
+		result = prime * result + maxDaysWeek;
+		result = prime * result + maxHoursDay;
+		result = prime * result + maxHoursWeek;
+		result = prime * result
+				+ ((mobilePhone == null) ? 0 : mobilePhone.hashCode());
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		result = prime * result + ((store == null) ? 0 : store.hashCode());
+		result = prime * result + ((zip == null) ? 0 : zip.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Employee))
+			return false;
+		final Employee other = (Employee) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (address2 == null) {
+			if (other.address2 != null)
+				return false;
+		} else if (!address2.equals(other.address2))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (hireDate == null) {
+			if (other.hireDate != null)
+				return false;
+		} else if (!hireDate.equals(other.hireDate))
+			return false;
+		if (homePhone == null) {
+			if (other.homePhone != null)
+				return false;
+		} else if (!homePhone.equals(other.homePhone))
+			return false;
+		if (manager != other.manager)
+			return false;
+		if (maxDaysWeek != other.maxDaysWeek)
+			return false;
+		if (maxHoursDay != other.maxHoursDay)
+			return false;
+		if (maxHoursWeek != other.maxHoursWeek)
+			return false;
+		if (mobilePhone == null) {
+			if (other.mobilePhone != null)
+				return false;
+		} else if (!mobilePhone.equals(other.mobilePhone))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (store == null) {
+			if (other.store != null)
+				return false;
+		} else if (!store.equals(other.store))
+			return false;
+		if (zip == null) {
+			if (other.zip != null)
+				return false;
+		} else if (!zip.equals(other.zip))
+			return false;
+		return true;
+	}
 	
 	
 	
--- src/main/java/com/laborguru/model/Store.java
+++ src/main/java/com/laborguru/model/Store.java
@@ -31,6 +31,46 @@ public class Store extends SpmObject {
 	public void setOffice(Office office) {
 		this.office = office;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((office == null) ? 0 : office.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Store))
+			return false;
+		final Store other = (Store) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (office == null) {
+			if (other.office != null)
+				return false;
+		} else if (!office.equals(other.office))
+			return false;
+		return true;
+	}
 	
 	
 }
