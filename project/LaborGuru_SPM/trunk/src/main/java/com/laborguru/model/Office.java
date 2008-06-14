be55eb5a7726b52ae65306b439bcca22f08852ec
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-06-14 23:39:44 +0000

    Added methods hash and equals.

--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/Office.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/Office.java
@@ -41,5 +41,50 @@ public class Office extends SpmObject {
 	public void setParentOffice(Office parentOffice) {
 		this.parentOffice = parentOffice;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((parentOffice == null) ? 0 : parentOffice.hashCode());
+		result = prime * result + type;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Office))
+			return false;
+		final Office other = (Office) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (parentOffice == null) {
+			if (other.parentOffice != null)
+				return false;
+		} else if (!parentOffice.equals(other.parentOffice))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	
 	
 }
--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/Position.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/Position.java
@@ -25,5 +25,44 @@ public class Position {
 	public void setName(String name) {
 		this.name = name;
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((store == null) ? 0 : store.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Position))
+			return false;
+		final Position other = (Position) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (store == null) {
+			if (other.store != null)
+				return false;
+		} else if (!store.equals(other.store))
+			return false;
+		return true;
+	}
+	
 	
 }
