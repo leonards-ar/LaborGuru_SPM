f11adcb87889db143e57dddf350b857a6c9510f9
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2008-06-15 23:20:21 +0000

    New elements added.

--- src/main/java/com/laborguru/model/User.java
+++ src/main/java/com/laborguru/model/User.java
@@ -1,5 +1,6 @@
 package com.laborguru.model;
 
+import java.util.Date;
 import java.util.Set;
 
 
@@ -21,9 +22,16 @@ public class User extends SpmObject {
 	
 	private String userName;
 	private String password;
+	private Integer status;
+	private String email;
 	private String name;
 	private String surname;
+	private Date lastLogon;
+	private Integer loginCount;
+	private Date creationDate;
+	private Date lastUpdateDate;
 	private Set<Profile> profiles;
+	
 	
 	
 	/* (non-Javadoc)
@@ -32,10 +40,26 @@ public class User extends SpmObject {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((creationDate == null) ? 0 : creationDate.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((lastLogon == null) ? 0 : lastLogon.hashCode());
+		result = prime * result
+				+ ((lastUpdateDate == null) ? 0 : lastUpdateDate.hashCode());
+		result = prime * result
+				+ ((loginCount == null) ? 0 : loginCount.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((profiles == null) ? 0 : profiles.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
-		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
 		return result;
 	}
 	/* (non-Javadoc)
@@ -43,36 +67,73 @@ public class User extends SpmObject {
 	 */
 	@Override
 	public boolean equals(Object obj) {
-		
 		if (this == obj)
 			return true;
-		
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
-		
 		if (!(obj instanceof User))
 			return false;
-		
 		final User other = (User) obj;
-		
+		if (creationDate == null) {
+			if (other.creationDate != null)
+				return false;
+		} else if (!creationDate.equals(other.creationDate))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastLogon == null) {
+			if (other.lastLogon != null)
+				return false;
+		} else if (!lastLogon.equals(other.lastLogon))
+			return false;
+		if (lastUpdateDate == null) {
+			if (other.lastUpdateDate != null)
+				return false;
+		} else if (!lastUpdateDate.equals(other.lastUpdateDate))
+			return false;
+		if (loginCount == null) {
+			if (other.loginCount != null)
+				return false;
+		} else if (!loginCount.equals(other.loginCount))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
-		
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (profiles == null) {
+			if (other.profiles != null)
+				return false;
+		} else if (!profiles.equals(other.profiles))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
 		if (surname == null) {
 			if (other.surname != null)
 				return false;
 		} else if (!surname.equals(other.surname))
 			return false;
-		
 		if (userName == null) {
 			if (other.userName != null)
 				return false;
 		} else if (!userName.equals(other.userName))
 			return false;
-		
 		return true;
 	}
 	/**
@@ -164,4 +225,78 @@ public class User extends SpmObject {
 
 		this.profiles.add(profile);
 	}
+	/**
+	 * @return the status
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the lastLogon
+	 */
+	public Date getLastLogon() {
+		return lastLogon;
+	}
+	/**
+	 * @param lastLogon the lastLogon to set
+	 */
+	public void setLastLogon(Date lastLogon) {
+		this.lastLogon = lastLogon;
+	}
+	/**
+	 * @return the loginCount
+	 */
+	public Integer getLoginCount() {
+		return loginCount;
+	}
+	/**
+	 * @param loginCount the loginCount to set
+	 */
+	public void setLoginCount(Integer loginCount) {
+		this.loginCount = loginCount;
+	}
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	/**
+	 * @return the lastUpdateDate
+	 */
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+	/**
+	 * @param lastUpdateDate the lastUpdateDate to set
+	 */
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+	
+	
 }
