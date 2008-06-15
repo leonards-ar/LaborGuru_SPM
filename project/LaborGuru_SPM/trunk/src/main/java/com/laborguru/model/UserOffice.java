d3cbb64ca486549ad1e8ea17a72030d8602a860f
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-06-15 00:36:43 +0000

    User that has an office associated.

---
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/UserOffice.java
@@ -0,0 +1,58 @@
+package com.laborguru.model;
+
+public class UserOffice extends User {
+
+	/**
+	 * Default serial version ID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Office office;
+
+	/**
+	 * @return the office
+	 */
+	public Office getOffice() {
+		return office;
+	}
+
+	/**
+	 * @param office the office to set
+	 */
+	public void setOffice(Office office) {
+		this.office = office;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((office == null) ? 0 : office.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof UserOffice))
+			return false;
+		final UserOffice other = (UserOffice) obj;
+		if (office == null) {
+			if (other.office != null)
+				return false;
+		} else if (!office.equals(other.office))
+			return false;
+		return true;
+	}
+	
+	
+}
