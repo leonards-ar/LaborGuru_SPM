fbebdc26919d1cd2dcb2bbfe84885dd296a8fbed
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-06-18 02:37:27 +0000

    Class that implements EmployeeService

---
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/service/employee/EmployeeServiceBean.java
@@ -0,0 +1,84 @@
+package com.laborguru.service.employee;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.laborguru.exception.ErrorEnum;
+import com.laborguru.exception.SpmUncheckedException;
+import com.laborguru.logger.DefaultSpmLogger;
+import com.laborguru.model.Employee;
+import com.laborguru.model.Store;
+import com.laborguru.service.employee.dao.EmployeeDao;
+
+public class EmployeeServiceBean implements EmployeeService {
+
+	private static final DefaultSpmLogger spmLog = DefaultSpmLogger.getInstance();
+	
+	EmployeeDao employeeDao;
+	
+	/* (non-Javadoc)
+	 * @see com.laborguru.service.employee.EmployeeService#delete(com.laborguru.model.Employee)
+	 */
+	public void delete(Employee employee) {
+		if(employee == null) {
+			spmLog.errorLog("error.null.param", new Object[]{"employee"});
+			throw new IllegalArgumentException("param is null");
+		}
+		employeeDao.delete(employee);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.laborguru.service.employee.EmployeeService#getEmployeeById(com.laborguru.model.Employee)
+	 */
+	public Employee getEmployeeById(Employee employee) {
+		if(employee == null) {
+			spmLog.errorLog("error.null.param", new Object[]{"employee"});
+			throw new IllegalArgumentException("param is null");
+		}
+		return employeeDao.getEmployeeById(employee);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.laborguru.service.employee.EmployeeService#getEmployeesByStore(com.laborguru.model.Store)
+	 */
+	public List<Employee> getEmployeesByStore(Store store) {
+		if(store == null) {
+			spmLog.errorLog("error.null.param", new Object[]{"store"});
+			throw new IllegalArgumentException("param is null");
+		}
+		return employeeDao.getEmployeesByStore(store);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.laborguru.service.employee.EmployeeService#save(com.laborguru.model.Employee)
+	 */
+	public Employee save(Employee employee) {
+		try{
+			if(employee == null) {
+				spmLog.errorLog("error.null.param", new Object[]{"employee"});
+				throw new IllegalArgumentException("param is null");				
+			}
+			return employeeDao.save(employee);
+		} catch(DataAccessException e) {
+			spmLog.errorLog("Error saving employee...", e);
+			throw new SpmUncheckedException(e, e.getMessage(), ErrorEnum.GENERIC_DATABASE_ERROR);
+		}
+	}
+
+	/**
+	 * @return the employeeDao
+	 */
+	public EmployeeDao getEmployeeDao() {
+		return employeeDao;
+	}
+
+	/**
+	 * @param employeeDao the employeeDao to set
+	 */
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+}
