3f6be8cf897911dbd7e2c6fd94ecdc18effe3281
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-06-25 02:41:32 +0000

    Adding the logout Action.

---
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/action/login/LogoutAction.java
@@ -0,0 +1,15 @@
+package com.laborguru.action.login;
+
+import org.apache.struts2.dispatcher.SessionMap;
+
+import com.laborguru.action.SpmAction;
+import com.laborguru.action.SpmActionResult;
+
+@SuppressWarnings("serial")
+public class LogoutAction extends SpmAction {
+
+	public String execute() {
+		((SessionMap)super.getSession()).invalidate();
+		return SpmActionResult.LOGOUT.getResult();
+	}
+}
