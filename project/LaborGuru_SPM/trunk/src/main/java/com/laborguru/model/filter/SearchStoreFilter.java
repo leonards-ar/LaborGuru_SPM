c1c3edc35a6282dda2e7348c464037217f5eca2a
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-07-02 03:11:05 +0000

    adding search support for store

--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/filter/SearchStoreFilter.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/filter/SearchStoreFilter.java
@@ -1,8 +1,9 @@
 package com.laborguru.model.filter;
 
-import com.laborguru.model.Area;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.laborguru.model.Customer;
-import com.laborguru.model.Region;
 import com.laborguru.model.SpmObject;
 
 
@@ -21,9 +22,40 @@ public class SearchStoreFilter extends SpmObject {
 	private static final long serialVersionUID = 1L;
 	
 	private Customer customer;
-	private Region region;
-	private Area area;
-	
+	private String name;
+	private String code;
+		
+	public String getHql(){
+		
+		List<String> hqlParams = new ArrayList<String>();
+		
+		StringBuilder sb = new StringBuilder("from Store store");
+		
+		if (customer != null){
+			hqlParams.add("store.area.region.customer.id=" + customer.getId().toString());
+		}
+
+		if (code != null){
+			hqlParams.add("store.code like '%"+code+"%'");
+		}
+		
+		if (name != null){
+			hqlParams.add("store.name like '%"+name+"%'");
+		}
+		
+		int i=0;
+		for (String param: hqlParams){
+			if (i==0){
+				sb.append(" where " + param);
+			}else {
+				sb.append(" and " + param);
+			}
+			
+			i++;
+		}
+		
+		return sb.toString();
+	}
 	
 	/**
 	 * @return the customer
@@ -40,36 +72,35 @@ public class SearchStoreFilter extends SpmObject {
 		this.customer = customer;
 	}
 
-
 	/**
-	 * @return the region
+	 * @return the name
 	 */
-	public Region getRegion() {
-		return region;
+	public String getName() {
+		return name;
 	}
 
 
 	/**
-	 * @param region the region to set
+	 * @param name the name to set
 	 */
-	public void setRegion(Region region) {
-		this.region = region;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 
 	/**
-	 * @return the area
+	 * @return the code
 	 */
-	public Area getArea() {
-		return area;
+	public String getCode() {
+		return code;
 	}
 
 
 	/**
-	 * @param area the area to set
+	 * @param code the code to set
 	 */
-	public void setArea(Area area) {
-		this.area = area;
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 
@@ -78,7 +109,7 @@ public class SearchStoreFilter extends SpmObject {
 	 * @return True when there is at least one attribute set on the filter, otherwise false.
 	 */
 	public boolean isFilled() {		
-		return (this.customer != null) || (this.region != null) || (this.area != null);
+		return (this.customer != null) || (this.name != null) || (this.code != null);
 	}
 	
 }
