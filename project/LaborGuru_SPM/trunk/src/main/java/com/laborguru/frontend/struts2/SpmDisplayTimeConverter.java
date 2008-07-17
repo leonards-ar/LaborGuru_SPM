99c452c1e068f4ab8b6a1b339a0be47ea7d9c3ab
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-07-17 01:18:24 +0000

    adding store day part definition CRUD

--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/frontend/struts2/SpmDisplayTimeConverter.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/frontend/struts2/SpmDisplayTimeConverter.java
@@ -11,13 +11,24 @@ import org.apache.struts2.util.StrutsTypeConverter;
 import com.laborguru.action.utils.CustomValidators;
 import com.opensymphony.xwork2.util.TypeConversionException;
 
+/**
+ * Struts converter that handles the String-Date conversion for times
+ * in SPM. It is used on the store CRUD.
+ * 
+ * @author <a href="cnunezre@gmail.com">Cristian Nunez Rebolledo</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
 public class SpmDisplayTimeConverter extends StrutsTypeConverter {
 
 	static Logger log = Logger.getLogger(SpmDisplayTimeConverter.class);
 
 	private static final SimpleDateFormat DISPLAY_TIME_FORMAT = new SimpleDateFormat("HH:mm");
 
-	@Override
+	/**
+	 * @see org.apache.struts2.util.StrutsTypeConverter#convertFromString(java.util.Map, java.lang.String[], java.lang.Class)
+	 */
 	public Object convertFromString(Map context, String[] values, Class toClass) {
 		String time = values[0];
 		
@@ -36,7 +47,9 @@ public class SpmDisplayTimeConverter extends StrutsTypeConverter {
 	}
 
 
-	@Override
+	/**
+	 * @see org.apache.struts2.util.StrutsTypeConverter#convertToString(java.util.Map, java.lang.Object)
+	 */
 	public String convertToString(Map context, Object arg1) {
 		return dateToDisplayTime((Date)arg1);
 	}
