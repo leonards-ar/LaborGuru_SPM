78da49ac00eb286b57e40825a403ddb1cb7d28a0
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2008-08-02 02:04:05 +0000

    Changing Activity Sharing to show the Groups in the combo instead of the positions.

--- src/main/java/com/laborguru/action/store/ActivitySharingPrepareAction.java
+++ src/main/java/com/laborguru/action/store/ActivitySharingPrepareAction.java
@@ -1,6 +1,9 @@
 package com.laborguru.action.store;
 
+import java.util.List;
+
 import com.laborguru.model.Position;
+import com.laborguru.model.PositionGroup;
 
 
 /**
@@ -13,17 +16,21 @@ import com.laborguru.model.Position;
 @SuppressWarnings("serial")
 public class ActivitySharingPrepareAction extends StoreUtilizationBaseAction {
 
+	private List<PositionGroup> positionGroups;
+	
 	public ActivitySharingPrepareAction() {
 		
 	}
 
+	public void loadExtraInformation() {
+		setPositionGroups(getStore().getOrderedPositionGroups());
+	}
 	/**
 	 * Gets the destination Position and Sets a SharedPosition
 	 */
 	@Override
 	protected void setPositionUtilizationValues(Position src, Position dest) {
-		dest.setSharedPosition(getPositionById(src.getSharedPosition().getId()));
-		src.setSharedPosition(dest);
+		dest.setPositionGroup(getPositionGroupById(src.getPositionGroup().getId()));
 	}
 	
 	
@@ -32,12 +39,28 @@ public class ActivitySharingPrepareAction extends StoreUtilizationBaseAction {
 	 * @param id
 	 * @return
 	 */
-	private Position getPositionById(Integer id) {
-		for(Position position: getStore().getPositions()) {
-			if(position.getId().equals(id)) {
-				return position;
+	private PositionGroup getPositionGroupById(Integer id) {
+		for(PositionGroup positionGroup: getStore().getPositionGroups()) {
+			if(positionGroup.getId().equals(id)) {
+				return positionGroup;
 			}
 		}
 		return null;
 	}
+
+	/**
+	 * @return the positionGroup
+	 */
+	public List<PositionGroup> getPositionGroups() {
+		return positionGroups;
+	}
+
+	/**
+	 * @param positionGroup the positionGroup to set
+	 */
+	public void setPositionGroups(List<PositionGroup> positionGroup) {
+		this.positionGroups = positionGroup;
+	}
+	
+	
 }
