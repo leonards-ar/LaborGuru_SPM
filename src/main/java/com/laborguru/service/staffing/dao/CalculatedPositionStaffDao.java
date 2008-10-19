e6e07320fda497a0778cd0f146b65be0ff89e77b
Mariano Capurro <marianocapurro@users.noreply.github.com>
2008-10-19 22:23:13 +0000

    Start of staffing calculations

---
+++ src/main/java/com/laborguru/service/staffing/dao/CalculatedPositionStaffDao.java
@@ -0,0 +1,17 @@
+/*
+ * File name: CalculatedPositionStaffDao.java
+ * Creation date: 19/10/2008 16:55:31
+ * Copyright Mindpool
+ */
+package com.laborguru.service.staffing.dao;
+
+/**
+ *
+ * @author <a href="mcapurro@gmail.com">Mariano Capurro</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
+public interface CalculatedPositionStaffDao {
+
+}
---
+++ src/main/java/com/laborguru/service/staffing/dao/CalculatedPositionStaffDaoHibernate.java
@@ -0,0 +1,26 @@
+/*
+ * File name: CalculatedPositionStaffDaoHibernate.java
+ * Creation date: 19/10/2008 16:57:47
+ * Copyright Mindpool
+ */
+package com.laborguru.service.staffing.dao;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+/**
+ *
+ * @author <a href="mcapurro@gmail.com">Mariano Capurro</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
+public class CalculatedPositionStaffDaoHibernate extends HibernateDaoSupport implements CalculatedPositionStaffDao {
+
+	/**
+	 * 
+	 */
+	public CalculatedPositionStaffDaoHibernate() {
+	}
+
+}
