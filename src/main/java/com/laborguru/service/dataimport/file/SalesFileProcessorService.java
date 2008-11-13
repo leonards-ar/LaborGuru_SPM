6cf31dbf856fc6073d20ba21e223b13e2584d507
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2008-11-13 00:36:57 +0000

    adding filename to the upload processor interface

--- src/main/java/com/laborguru/service/dataimport/file/SalesFileProcessorService.java
+++ src/main/java/com/laborguru/service/dataimport/file/SalesFileProcessorService.java
@@ -15,11 +15,14 @@ import com.laborguru.model.UploadFile;
 public interface SalesFileProcessorService {
 	
 	/**
-	 * This method process the file passed as parameter, creates and persist an Upload File entity and the sales records contained in the file.
-	 * @param file
+	 * This method process the file passed as parameter, creates and persist an UploadFile object and the sales records contained in the file.
+	 * @param file: the file to upload and save
+	 * @param uploadFile: Optional helper objec used to pass information to the service. The values defined in the object are going to be used when 
+	 * creating the UploadFile instance. If null method defaults are used. 	 
+	 * 
 	 * @return The upload file entity created.
 	 */
-	UploadFile processAndSaveFile(File file);
+	UploadFile processAndSaveFile(File file, UploadFile uploadFile);
 	
 	/**
 	 * Sets the file parser to use when processing the file
