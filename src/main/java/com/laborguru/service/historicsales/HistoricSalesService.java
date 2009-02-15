89b97346896d73b45bdc5d4f5166cc8f55845651
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2009-02-15 22:26:40 +0000

    Adding half hour service for historic sales

--- src/main/java/com/laborguru/service/historicsales/HistoricSalesService.java
+++ src/main/java/com/laborguru/service/historicsales/HistoricSalesService.java
@@ -1,6 +1,10 @@
 package com.laborguru.service.historicsales;
 
+import java.util.Date;
+
+import com.laborguru.model.DailyHistoricSales;
 import com.laborguru.model.HistoricSales;
+import com.laborguru.model.Store;
 
 /**
  * Handles historic sales services
@@ -17,4 +21,13 @@ public interface HistoricSalesService {
 	 * @param The record to store.
 	 */
 	void save(HistoricSales hs);
+	
+	/**
+	 * Returns a DailyHistoricSales object populated with the sales information for the date passed as parameter.
+	 * 
+	 * @param store
+	 * @param date
+	 * @return The DailyHistoricSales
+	 */
+	DailyHistoricSales getDailyHistoricSalesByDate(Store store, Date date);
 }
--- src/main/java/com/laborguru/service/historicsales/HistoricSalesServiceBean.java
+++ src/main/java/com/laborguru/service/historicsales/HistoricSalesServiceBean.java
@@ -1,6 +1,10 @@
 package com.laborguru.service.historicsales;
 
+import java.util.Date;
+
+import com.laborguru.model.DailyHistoricSales;
 import com.laborguru.model.HistoricSales;
+import com.laborguru.model.Store;
 
 /**
  * Historic Sales Services Bean
@@ -14,6 +18,10 @@ public class HistoricSalesServiceBean implements HistoricSalesService {
 
 
 	public void save(HistoricSales hs) {
+	}
+
+	public DailyHistoricSales getDailyHistoricSalesByDate(Store store, Date date) {
+		return null;
 	}
 
 }
