3e08416eeae945cfd546f854ebc036f1d56a3bd7
Mariano Capurro <marianocapurro@users.noreply.github.com>
2009-06-21 15:35:52 +0000

    Fixed login and forgot password

--- src/main/java/com/laborguru/action/login/ForgotPasswordAction.java
+++ src/main/java/com/laborguru/action/login/ForgotPasswordAction.java
@@ -5,6 +5,9 @@
  */
 package com.laborguru.action.login;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
 import com.laborguru.action.SpmAction;
 import com.laborguru.action.SpmActionResult;
 import com.laborguru.exception.SpmCheckedException;
@@ -20,6 +23,8 @@ import com.laborguru.service.user.UserService;
  *
  */
 public class ForgotPasswordAction extends SpmAction {
+	private static final Logger log = Logger.getLogger(ForgotPasswordAction.class);
+
 	/**
 	 * 
 	 */
@@ -46,11 +51,15 @@ public class ForgotPasswordAction extends SpmAction {
 				user = getUserService().getUserByUserName(user);
 				
 				if(user != null) {
-					user = getUserService().resetPassword(user);
-					
-					notifyNewPassword(user);
-					
-					addActionMessage(getText("login.forgot_password.message_sent", new String[] {user.getEmail()}));
+					if(!StringUtils.isEmpty(user.getEmail())) {
+						user = getUserService().resetPassword(user);
+						
+						notifyNewPassword(user);
+						
+						addActionMessage(getText("login.forgot_password.message_sent", new String[] {user.getEmail()}));
+					} else {
+						addActionError(getText("login.forgot_password.email.empty", new String[] {getUsername()}));
+					}
 				} else {
 					addActionError(getText("login.forgot_password.username.invalid", new String[] {getUsername()}));
 				}
@@ -58,22 +67,28 @@ public class ForgotPasswordAction extends SpmAction {
 				addActionError(getText("login.forgot_password.username.empty"));
 			}
 		} catch(SpmCheckedException ex) {
+			log.error(ex.getMessage(), ex);
 			addActionError(ex.getErrorMessage());
 		}
 		
 		return SpmActionResult.LOGIN.getResult();
 	}
 	
+
 	/**
 	 * 
 	 * @param user
+	 * @throws SpmCheckedException
 	 */
-	private void notifyNewPassword(User user) {
-		String to = user.getEmail();
-		String subject = getText("login.forgot_password.email.subject", new String[] {user.getFullName(), user.getUserName()});
-		String body = getText("login.forgot_password.email.body", new String[] {user.getFullName(), user.getUserName(), user.getPassword(), getRemoteAddress()});
-		//:TODO: Uncomment and test :). Set correct parameters in spm.properties
-		//getEmailService().sendEmail(new String[]{to}, null, subject, body);
+	private void notifyNewPassword(User user) throws SpmCheckedException {
+		try {
+			String to = user.getEmail();
+			String subject = getText("login.forgot_password.email.subject", new String[] {user.getFullName(), user.getUserName()});
+			String body = getText("login.forgot_password.email.body", new String[] {user.getFullName(), user.getUserName(), user.getPassword(), getRemoteAddress()});
+			getEmailService().sendEmail(new String[]{to}, null, subject, body);
+		} catch(Throwable ex) {
+			log.error("Cannot send new password to user [" + user.getUserName() + "]", ex);
+		}
 	}
 	
 	/**
