9cd76d8071b36b37aaf139ab71b58a277c104a14
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2009-11-02 04:41:51 +0000

    Added interface Location. Added a set of stores to the Area class. Added a method getStores to Region.

---
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/Location.java
@@ -0,0 +1,20 @@
+package com.laborguru.model;
+
+/**
+ * Interfaces for Customer, Region and Area
+ * @author <a href="fbarreraoro@gmail.com">Federico Barrera Oro</a>
+ * @version 1.1
+ * @since SPM 1.1
+ *
+ */
+public interface Location {
+
+	Integer getId();
+	
+	void setId(Integer id);
+	
+	String getName();
+	
+	void setName(String name);
+	
+}
