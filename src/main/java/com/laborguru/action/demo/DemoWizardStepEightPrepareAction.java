6a5452350fd3d004519d30c4be6735768d4607f5
Mariano Capurro <marianocapurro@users.noreply.github.com>
2011-02-05 20:18:27 +0000

    Added email notification capabilities to demo wizard.

--- src/main/java/com/laborguru/action/demo/DemoWizardStepEightPrepareAction.java
+++ src/main/java/com/laborguru/action/demo/DemoWizardStepEightPrepareAction.java
@@ -3,9 +3,13 @@
  */
 package com.laborguru.action.demo;
 
+import org.apache.log4j.Logger;
+
 import com.laborguru.action.SpmActionResult;
 import com.laborguru.exception.SpmCheckedException;
+import com.laborguru.model.Employee;
 import com.laborguru.model.Store;
+import com.laborguru.service.email.EmailService;
 
 /**
  * @author mariano
@@ -13,6 +17,10 @@ import com.laborguru.model.Store;
  */
 public class DemoWizardStepEightPrepareAction extends DemoWizardBaseAction {
 	private static final long serialVersionUID = 1L;
+
+	private static final Logger log = Logger.getLogger(DemoWizardStepEightPrepareAction.class);
+	
+	private EmailService emailService;
 
 	/**
 	 * 
@@ -42,6 +50,8 @@ public class DemoWizardStepEightPrepareAction extends DemoWizardBaseAction {
 			setStoreName(store.getName());
 			setEmployeeUserName(getEmployee().getUserName());
 
+			notify(store, getEmployee());
+			
 			getSession().remove(STORE_SESSION_KEY);
 			getSession().remove(EMPLOYEE_SESSION_KEY);
 			
@@ -50,5 +60,37 @@ public class DemoWizardStepEightPrepareAction extends DemoWizardBaseAction {
 			addActionError(ex.getErrorMessage());
 			return SpmActionResult.INPUT.getResult();
 		}
+	}
+	
+	/**
+	 * 
+	 * @param store
+	 * @param employee
+	 */
+	private void notify(Store store, Employee employee) {
+		try {
+			String to = getText("demo.wizard.notify.to");
+			String subject = getText("demo.wizard.notify.subject", new String[] {store.getName(), employee.getUserName()});
+			String body = getText("demo.wizard.notify.body", new String[] {store.getName(), employee.getUserName()});
+			getEmailService().sendEmail(new String[]{to}, null, subject, body);
+		} catch(Throwable ex) {
+			log.error("Cannot send new demo store creation [" + store + "]", ex);
+		}		
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public EmailService getEmailService() {
+		return emailService;
+	}
+
+	/**
+	 * 
+	 * @param emailService
+	 */
+	public void setEmailService(EmailService emailService) {
+		this.emailService = emailService;
 	}
 }
