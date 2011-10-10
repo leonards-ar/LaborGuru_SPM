694d9cf46096fdd40d575c1bc261dcf14a055bb1
Mariano Capurro <marianocapurro@users.noreply.github.com>
2011-10-10 22:24:00 +0000

    Fixed FS#297.

--- src/main/java/com/laborguru/service/schedule/ScheduleServiceBean.java
+++ src/main/java/com/laborguru/service/schedule/ScheduleServiceBean.java
@@ -7,13 +7,17 @@ package com.laborguru.service.schedule;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.laborguru.model.EmployeeSchedule;
+import com.laborguru.model.Shift;
 import com.laborguru.model.Store;
 import com.laborguru.model.StoreSchedule;
 import com.laborguru.service.schedule.dao.ScheduleDao;
+import com.laborguru.util.CalendarUtils;
 
 /**
  *
@@ -126,6 +130,84 @@ public class ScheduleServiceBean implements ScheduleService {
 				employeeSchedule.reindexShifts();
 			}			
 			getScheduleDao().save(aSchedule);
+		}
+	}
+
+	public StoreSchedule copyScheduleTo(StoreSchedule sourceSchedule, Date date, StoreSchedule destinationNextDaySchedule) {
+		StoreSchedule destinationSchedule = getStoreScheduleByDate(sourceSchedule.getStore(), date);
+		if(destinationSchedule == null) {
+			destinationSchedule = new StoreSchedule();
+			destinationSchedule.setDay(date);
+			destinationSchedule.setStore(sourceSchedule.getStore());
+		}
+		
+		copyEmployeeSchedule(sourceSchedule, destinationSchedule, destinationNextDaySchedule);
+		
+		return destinationSchedule;
+	}
+	
+	private void copyEmployeeSchedule(StoreSchedule sourceSchedule, StoreSchedule destinationSchedule, StoreSchedule destinationNextDaySchedule) {
+		prepareEmployeeSchedule(destinationSchedule, sourceSchedule.getEmployeeSchedules().size());
+		
+		Iterator<EmployeeSchedule> srcIt = sourceSchedule.getEmployeeSchedules().iterator();
+		Iterator<EmployeeSchedule> destIt = destinationSchedule.getEmployeeSchedules().iterator();
+		while(srcIt.hasNext() && destIt.hasNext()) {
+			EmployeeSchedule src = srcIt.next();
+			EmployeeSchedule dest = destIt.next();
+			
+			dest.setEmployee(src.getEmployee());
+			dest.setStoreSchedule(destinationSchedule);
+			
+			copyShifts(src, dest, destinationNextDaySchedule.getEmployeeSchedule(src.getEmployee()));
+		}
+	}
+
+	private void copyShifts(EmployeeSchedule sourceEmployeeSchedule, EmployeeSchedule destinationEmployeeSchedule, EmployeeSchedule destinationNextDayEmployeeSchedule) {
+		prepareShifts(destinationEmployeeSchedule, sourceEmployeeSchedule.getShifts().size());
+		
+		for(int i = 0; i < sourceEmployeeSchedule.getShifts().size(); i++) {
+			Shift src = sourceEmployeeSchedule.getShifts().get(i);
+			Shift dest = destinationEmployeeSchedule.getShifts().get(i);
+			
+			dest.setClosingHours(src.getClosingHours());
+			dest.setFromHour(src.getFromHour());
+			dest.setOpeningHours(src.getOpeningHours());
+			dest.setPosition(src.getPosition());
+			dest.setServiceHours(src.getServiceHours());
+			dest.setToHour(src.getToHour());
+			
+			if(src.hasContiguousShift()) {
+				Shift candidateShif = destinationNextDayEmployeeSchedule.getFirstShiftFor(src.getPosition());
+				if(CalendarUtils.equalsTime(candidateShif.getFromHour(), dest.getToHour())) {
+					dest.setContiguousShift(candidateShif);
+				}
+			}
+		}
+	}
+	
+	private void prepareShifts(EmployeeSchedule destinationEmployeeSchedule, int size) {
+		if(size > destinationEmployeeSchedule.getShifts().size()) {
+			for(int i = 0; i < size - destinationEmployeeSchedule.getShifts().size(); i++) {
+				destinationEmployeeSchedule.addShift(new Shift());
+			}			
+		} else if(size < destinationEmployeeSchedule.getShifts().size()) {
+			for(int i = 0; i < destinationEmployeeSchedule.getShifts().size() - size; i++) {
+				destinationEmployeeSchedule.removeLastShift();
+			}				
+		}
+	}
+	
+	private void prepareEmployeeSchedule(StoreSchedule destinationSchedule, int size) {
+		if(size > destinationSchedule.getEmployeeSchedules().size()) {
+			for(int i = 0; i < size - destinationSchedule.getEmployeeSchedules().size(); i++) {
+				destinationSchedule.getEmployeeSchedules().add(new EmployeeSchedule());
+			}
+		} else if(size < destinationSchedule.getEmployeeSchedules().size()) {
+			Iterator<EmployeeSchedule> it = destinationSchedule.getEmployeeSchedules().iterator();
+			for(int i = 0; i < destinationSchedule.getEmployeeSchedules().size() - size; i++) {
+				it.next();
+				it.remove();
+			}
 		}
 	}
 }
