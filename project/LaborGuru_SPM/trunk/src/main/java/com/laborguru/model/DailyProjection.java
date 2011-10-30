a5ccf03cd3a829174b6682e6eacc5c220afe4294
mariano <mariano@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2011-10-30 22:26:20 +0000

    The method getStore in the toString method was throwing an exception

--- project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/DailyProjection.java
+++ project/LaborGuru_SPM/trunk/src/main/java/com/laborguru/model/DailyProjection.java
@@ -83,7 +83,6 @@ public class DailyProjection extends DailySalesValue {
 	public String toString() {
 		return new ToStringBuilder(this, DEFAULT_TO_STRING_STYLE)
 	   	.append("id" , getId())
-	   	.append("store", getStore())
 	   	.append("date",getSalesDate())
 	   	.append("startingTime",getStartingTime())
 	   	.toString();		
