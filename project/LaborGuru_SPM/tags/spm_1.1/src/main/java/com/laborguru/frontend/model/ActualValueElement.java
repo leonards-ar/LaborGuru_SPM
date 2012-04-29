a44750b843e17ca5b040559f8084b27781eaa07c
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2012-04-29 14:36:10 +0000

    New Release

---
+++ project/LaborGuru_SPM/tags/spm_1.1/src/main/java/com/laborguru/frontend/model/ActualValueElement.java
@@ -0,0 +1,80 @@
+package com.laborguru.frontend.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import com.laborguru.util.CalendarUtils;
+import com.laborguru.util.NumberUtils;
+import com.laborguru.util.SpmConstants;
+
+public class ActualValueElement {
+
+	private BigDecimal mainValue;
+	private Double hours;
+	private Date date;
+	
+	/**
+	 * @return the mainValue
+	 */
+	public BigDecimal getMainValue() {
+		if (this.mainValue == null){
+			setMainValue(new BigDecimal(SpmConstants.INIT_VALUE_ZERO));
+		}
+		return this.mainValue;
+	}
+	/**
+	 * @param mainValue the mainValue to set
+	 */
+	public void setMainValue(BigDecimal mainValue) {
+		this.mainValue = mainValue;
+	}
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Boolean getEditable(){
+		return (CalendarUtils.todayWithoutTime().compareTo(getDate()) >= 0);
+	}	
+
+	public int getMainValueToDisplay(){
+		return NumberUtils.bigDecimalToInt(mainValue);
+	}
+	
+	public String toString(){
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+	   	.append("mainValue" , getMainValue())
+	   	.append("hours" , getHours())
+	   	.append("date",getDate())
+	   	.toString();		
+		
+	}
+	/**
+	 * @return the actualHours
+	 */
+	public Double getHours() {
+		if (this.hours == null)
+		{
+			setHours(0.0);
+		}
+		
+		return hours;
+	}
+	/**
+	 * @param actualHours the actualHours to set
+	 */
+	public void setHours(Double actualHours) {
+		this.hours = actualHours;
+	}
+}
---
+++ project/LaborGuru_SPM/tags/spm_1.1/src/main/java/com/laborguru/model/HalfHourHistoricSales.java
@@ -0,0 +1,167 @@
+package com.laborguru.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import com.laborguru.util.CalendarUtils;
+
+
+/**
+ * Deals with Half an hour historic sales behaviour.
+ * 
+ * @author <a href="cnunezre@gmail.com">Cristian Nunez Rebolledo</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
+public class HalfHourHistoricSales extends HalfHourSalesValue implements Comparable<HalfHourHistoricSales> {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private BigDecimal value;
+	private DailyHistoricSales dailyHistoricSales;
+	private Date time;
+	
+	
+	public HalfHourHistoricSales(){
+		
+	}
+		
+	/**
+	 * @param obj
+	 * @return
+	 * @see com.laborguru.model.SpmObject#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;		
+
+		final HalfHourHistoricSales other = (HalfHourHistoricSales) obj;
+		
+		return new EqualsBuilder()
+		.append(getValue(), other.getValue())
+		.append(getTime(), other.getTime())
+		.isEquals();	
+	}
+
+	/**
+	 * @return
+	 * @see com.laborguru.model.SpmObject#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37)
+		.append(getValue())
+		.append(getTime())
+		.toHashCode();
+	}
+
+	/**
+	 * @return
+	 * @see com.laborguru.model.SpmObject#toString()
+	 */
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, DEFAULT_TO_STRING_STYLE)
+	   	.append("value",getValue())
+	   	.append("time",getTime())
+	   	.toString();	
+	}
+
+	/**
+	 * @param dailyHistoricSales
+	 */
+	public void setDailyHistoricSales(DailyHistoricSales dailyHistoricSales) {
+		this.dailyHistoricSales = dailyHistoricSales;
+	}
+
+	/**
+	 * @return
+	 */
+	public DailyHistoricSales getDailyHistoricSales() {
+		return dailyHistoricSales;
+	}	
+
+
+	/**
+	 * @return the value
+	 */
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	
+	/**
+	 * @return the time
+	 */
+	public Date getTime() {
+		return time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	
+	/**
+	 * Sets time 
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		setTime(CalendarUtils.displayTimeToDate(time));
+	}	
+	
+	/**
+	 * Returns time as string if not null.
+	 * @return the time as string
+	 */
+	public String getTimeAsString(){
+		
+		if (time != null)
+			return CalendarUtils.dateToDisplayTime(this.getTime());
+		
+		return null;
+	}
+
+	/**
+	 * Compares to HalfHourHistoricSales by time
+	 * @param object
+	 * @return
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	public int compareTo(HalfHourHistoricSales object) {
+
+		if (object == null){
+			return 1;
+		}
+		
+		Date anotherDate = object.getTime();
+		
+		if (getTime() != null){
+			return (anotherDate != null)? getTime().compareTo(anotherDate): 1;
+		}
+		
+		return (anotherDate != null)?-1:0;
+	}
+}
