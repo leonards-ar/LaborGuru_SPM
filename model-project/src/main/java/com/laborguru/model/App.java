837129d2e208525c011f1af21cc27c01c8178614
federico-barreraoro <federico.barreraoro@mulesoft.com>
2018-04-01 23:33:37 -0300

    Adding model as jar

---
+++ model-project/src/main/java/com/laborguru/model/App.java
@@ -0,0 +1,13 @@
+package com.mindpool.spm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
